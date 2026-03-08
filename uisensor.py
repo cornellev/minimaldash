aafb3a294f0b800d0d22c41fa270d6827a6cfa6f
Katie Xiao <jx385@cornell.edu>
2026-03-08 01:15:08 -0500

    git submodules

--- uisensor.py
+++ uisensor.py
@@ -7,7 +7,7 @@ from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 import math
 from collections import deque
-from read_shm import SensorShmReader
+from uc26_sensor_reader.read_shm import SensorShmReader
 WINDOW_DURATION = 10.0# seconds for rolling efficiency window
 TIMESTAMP_WRAP= 70 * 60# seconds before timestamp rolls over
 METER_TO_MILE= 1 / 1609.344
